4aaa5b788240b66131484c003c494fe0e6ea8a1c
Jian <jay.sun71503@gmail.com>
2022-08-01 20:28:37 +1000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,11 @@ import ListComponent from './components/ListComponent'
 import Login from './components/Login'
 import CssBaseline from '@mui/material/CssBaseline';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import Grid from '@mui/material/Grid';
 
 const theme = createTheme();
 
 function App() {
   const [loggedin, setLoggedin] = useState(false)
-  const [isLoading, setIsLoading] = useState(false);
   
   return (
     <ThemeProvider theme={theme}>
