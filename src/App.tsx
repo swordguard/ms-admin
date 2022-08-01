890cab5fc5cfcf7ff7c20e427e9dad546569d324
Jian <jay.sun71503@gmail.com>
2022-08-01 20:27:09 +1000

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,21 @@
+import { useState } from 'react';
 import ListComponent from './components/ListComponent'
+import Login from './components/Login'
+import CssBaseline from '@mui/material/CssBaseline';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import Grid from '@mui/material/Grid';
+
+const theme = createTheme();
 
 function App() {
+  const [loggedin, setLoggedin] = useState(false)
+  const [isLoading, setIsLoading] = useState(false);
+  
   return (
-    <ListComponent />
+    <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {loggedin ? <ListComponent setLoggedin={setLoggedin}/> : <Login setLoggedin={setLoggedin}/>}
+    </ThemeProvider>
   );
 }
 
--- src/components/ListComponent/index.js
+++ src/components/ListComponent/index.js
@@ -17,8 +17,13 @@ import Button from '@mui/material/Button';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
 import { FEEDBACK_ENDPOINT } from '../constants/constants'
+import AppBar from '@mui/material/AppBar';
+import HighQualityIcon from '@mui/icons-material/HighQuality';
+// import Typography from '@mui/material/Typography';
+import Toolbar from '@mui/material/Toolbar';
+import Grid from '@mui/material/Grid';
 
-function ListComponent() {
+function ListComponent({setLoggedin}) {
   const [feedbackList, setFeedbackList] = useState([])
   const [openConfirmation, setOpenConfirmation] = useState(false);
   const [toBeDeleted, setToBeDeleted] = useState('');
@@ -63,66 +68,80 @@ function ListComponent() {
     setOpenConfirmation(false);
     setToBeDeleted('')
   };
-
-  if (isLoading) {
-    return <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-      <CircularProgress />
-    </Box>
-  }
-
+  
   return (
-    <TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Date time</TableCell>
-            <TableCell align="right">Full Name</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Comments</TableCell>
-            <TableCell align="right">Action</TableCell>
-          </TableRow>
-        </TableHead>
-          <TableBody>
-            {feedbackList.map((row) => (
-            <TableRow
-              key={row.backupId}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+    <Grid>
+        <AppBar position="relative">
+            <Toolbar sx={{textAlign: 'right'}}>
+                <HighQualityIcon sx={{ mr: 2 }} />
+                <Button
+                type="submit"
+                variant="contained"
+                color="secondary"
+                sx={{ mt: 3, mb: 2 }}
+                onClick={() => setLoggedin(false)}
+              >
+                Sign Out
+              </Button>
+            </Toolbar>
+        </AppBar>
+        {isLoading ? <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+            <CircularProgress />
+            </Box> :
+            <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                <TableHead>
+                <TableRow>
+                    <TableCell>Date time</TableCell>
+                    <TableCell align="right">Full Name</TableCell>
+                    <TableCell align="right">Email</TableCell>
+                    <TableCell align="right">Comments</TableCell>
+                    <TableCell align="right">Action</TableCell>
+                </TableRow>
+                </TableHead>
+                <TableBody>
+                    {feedbackList.map((row) => (
+                    <TableRow
+                    key={row.backupId}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+                    >
+                        <TableCell component="th" scope="row">
+                        {new Date(Number(row.backupId.substring(0, row.backupId.indexOf('-')))).toUTCString()}
+                    </TableCell>
+                    <TableCell component="th" scope="row">
+                        {`${row.firstName} ${row.lastName}`}
+                    </TableCell>
+                    <TableCell align="right">{row.email}</TableCell>
+                    <TableCell align="right">{row.comments}</TableCell>
+                    <TableCell align="right"><DeleteIcon onClick={(e) => handleConfirmation(e, row.backupId)}/></TableCell>
+                    </TableRow>
+                ))}
+                </TableBody>
+            </Table>
+            <Dialog
+                open={openConfirmation}
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
             >
-                <TableCell component="th" scope="row">
-                {new Date(Number(row.backupId.substring(0, row.backupId.indexOf('-')))).toUTCString()}
-              </TableCell>
-              <TableCell component="th" scope="row">
-                {`${row.firstName} ${row.lastName}`}
-              </TableCell>
-              <TableCell align="right">{row.email}</TableCell>
-              <TableCell align="right">{row.comments}</TableCell>
-              <TableCell align="right"><DeleteIcon onClick={(e) => handleConfirmation(e, row.backupId)}/></TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <Dialog
-        open={openConfirmation}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        <DialogTitle id="alert-dialog-title">
-          {"Delete confirmation"}
-        </DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-description">
-            Are you sure to delete it?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} disabled={isDeleting}>No</Button>
-          <Button onClick={() => deleteRecordById(toBeDeleted)} autoFocus value='Y' disabled={isDeleting}>
-            Yes
-          </Button>
-        </DialogActions>
-      </Dialog>
-    </TableContainer>
+                <DialogTitle id="alert-dialog-title">
+                {"Delete confirmation"}
+                </DialogTitle>
+                <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Are you sure to delete it?
+                </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                <Button onClick={handleClose} disabled={isDeleting}>No</Button>
+                <Button onClick={() => deleteRecordById(toBeDeleted)} autoFocus value='Y' disabled={isDeleting}>
+                    Yes
+                </Button>
+                </DialogActions>
+            </Dialog>
+            </TableContainer>
+        }
+    </Grid>
   );
 }
 
